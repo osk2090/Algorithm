23a13439f4cf09a501e290bd384dd6e1f1021288
osk2090 <osk2090@naver.com>
2022-02-24 23:29:21 +0900

    대소문자변환

---
+++ src/대소문자변환/Main.java
@@ -0,0 +1,24 @@
+package 대소문자변환;
+
+import java.util.Scanner;
+
+public class Main {
+    public String solution(String str) {
+        String answer = "";
+        for (char x : str.toCharArray()) {
+            if (Character.isLowerCase(x)) {
+                answer += Character.toUpperCase(x);
+            } else {
+                answer += Character.toLowerCase(x);
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main m = new Main();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(m.solution(str));
+    }
+}
