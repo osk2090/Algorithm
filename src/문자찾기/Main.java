147d7f5b4d5bb51a145ab2e569b33b4a716c2b6e
osk2090 <osk2090@naver.com>
2022-02-24 22:11:16 +0900

    first commit

---
+++ src/문자찾기/Main.java
@@ -0,0 +1,28 @@
+package 문자찾기;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public int solution(String str, char t) {
+        int answer = 0;
+        str = str.toUpperCase();
+        t = Character.toUpperCase(t);
+        for (char x : str.toCharArray()) {
+            if (x == t) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Main main = new Main();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char c = sc.next().charAt(0);
+
+        System.out.println(main.solution(str, c));
+    }
+}
