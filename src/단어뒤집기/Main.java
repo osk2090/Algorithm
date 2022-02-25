4230ba43327509aef3c8d3120bdc5d5033b65166
osk2090 <osk2090@naver.com>
2022-02-26 02:37:49 +0900

    단어뒤집기

---
+++ src/단어뒤집기/Main.java
@@ -0,0 +1,31 @@
+package 단어뒤집기;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public ArrayList<String> solution(int n, String[] str) {
+        ArrayList<String> answer = new ArrayList<>();
+
+        for (String x : str) {
+            String tmp = new StringBuilder(x).reverse().toString();
+            answer.add(tmp);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main m = new Main();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        String[] str = new String[n];
+        for (int i = 0; i < n; i++) {
+            str[i] = sc.next();
+        }
+
+        for (String x : m.solution(n, str)) {
+            System.out.println(x);
+        }
+    }
+}
